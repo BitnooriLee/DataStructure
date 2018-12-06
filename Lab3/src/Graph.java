80c21ebe06029818057b01afd09a72a379ef2538
bitnoori <bitnoori.lee@gmail.com>
2018-12-06 16:58:02 +0100

    add comment

--- Lab3/src/Graph.java
+++ Lab3/src/Graph.java
@@ -4,9 +4,9 @@ public class Graph { //class
 	public class Vertex implements Comparable<Vertex> { //class(Vertex < Graph)
 		// declare variable
 		public final String name;
-		public ArrayList<Edge> neighbours; // 현재의 Vortex에 이웃으로 연결되는 Edge를 저장
-		public LinkedList<Vertex> path; // 현재 node까지 오는데 최소 거리의 경로를 저장
-		public int minDistance = Integer.MAX_VALUE; // 현재 node까지 오는데 최소 거리를 저장
+		public ArrayList<Edge> neighbours; // Edge current Vortex to neighbours 
+		public LinkedList<Vertex> path; // min distance path to current node 
+		public int minDistance = Integer.MAX_VALUE; // min distance to current node 
 
         public Vertex(String name) { // constructor (Vertex)
 			//object 
@@ -15,7 +15,7 @@ public class Graph { //class
 			path = new LinkedList<Vertex>();
         }
         
-        @Override //to use toString ???? 
+        @Override //to use toString 
         public String toString() {
             return name;
         }
@@ -27,8 +27,8 @@ public class Graph { //class
 
 	public class Edge { // class (Edge < Graph)
 		// declare variable
-		public final Vertex dest; // 현재 node를 기준으로 Edge를 통해 연결될 목적지 node
-        public final int weight;
+		public final Vertex dest; // destination node from current node through the Edge
+        public final int weight; 
 
 		public Edge(Vertex dest, int weight) {
 			this.dest = dest;
@@ -37,7 +37,7 @@ public class Graph { //class
 			
 	}
 
-	private HashMap<String, Vertex> vertices; // type variable....?
+	private HashMap<String, Vertex> vertices; // type variable
 
 	public Graph() { // Constructor (Graph)
 		//object
@@ -49,10 +49,10 @@ public class Graph { //class
 	}
 
 	public void addEdge(String src, String dest, int weight) { // method(Graph)
-		Vertex s = vertices.get(src); // Edge의 방향: A -> B
+		Vertex s = vertices.get(src); // Edge: A -> B
 		s.neighbours.add(new Edge(vertices.get(dest), weight));
 
-		Vertex d = vertices.get(dest); // Edge의 방향: B -> A, Undirected이므로 이렇게 양방향에 같은 점수를 입력해 줌
+		Vertex d = vertices.get(dest); // Edge: B -> A, Undirected
 		d.neighbours.add(new Edge(vertices.get(src), weight));
     }
 	
@@ -64,7 +64,7 @@ public class Graph { //class
 	            this.vertices = vertices;
 	        }
 
-	        @Override //to use toString ???? 
+	        @Override //to use toString 
 	        public String toString() {
 	            return "totalDist: " + totalDist + ", vertices: " + vertices.toString();
 	        }
@@ -77,40 +77,40 @@ public class Graph { //class
 			return new Path(0, temp);
 		}
 		
-        for (String key: vertices.keySet()) { // 그래프 초기화, 밑에서 Dijkstra 알고리즘이 한바퀴 돌 때마다 minDistance와 path에 값들이 추가되기 때문에 새로 계산을 시작하기 전에 초기화
+        for (String key: vertices.keySet()) { // reset Graph, Everytime Dijkstra Algorithm executes, minDistance and path are added, 
+        									  // so reset before calculation
             vertices.get(key).minDistance = Integer.MAX_VALUE;
             vertices.get(key).path = new LinkedList<Vertex>();
         }
 
-        Vertex source = vertices.get(src); // 시작 node
-		source.minDistance = 0; // 시작 node이므로 최소거리는 0
+        Vertex source = vertices.get(src); // Start node
+		source.minDistance = 0; // Start node, minDistance = 0
 		PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>();
-		queue.add(source); // 현재 node부터 탐색 시작
+		queue.add(source); // current node
 
 		while (!queue.isEmpty()) {
-			Vertex u = queue.poll(); // 현재 node를 queue에서 꺼냄
-			for (Edge neighbour: u.neighbours) { // 현재 node의 이웃 node들을 살펴봄
-				int newDist = u.minDistance + neighbour.weight; // 현재 node에서 이웃 node로 연결되는 distance
+			Vertex u = queue.poll(); // pop current node from the queue
+			for (Edge neighbour: u.neighbours) { // check neighbour of current node
+				int newDist = u.minDistance + neighbour.weight; // distance from current to neighbour node
 
-				if (neighbour.dest.minDistance > newDist) { // 현재 node에서 이웃 node로 연결되는 distance가, 지금까지 계산된 이웃 node까지 가는 거리보다 작은 경우
-					queue.remove(neighbour.dest); // 이웃 node의 업데이트를 위해 일단 queue에서 지움
-					neighbour.dest.minDistance = newDist; // 새로 계산된 최단 거리를 입력
+				if (neighbour.dest.minDistance > newDist) { // current distance vs minDistance
+					queue.remove(neighbour.dest); // remove for update 
+					neighbour.dest.minDistance = newDist; // update distance
 
-					neighbour.dest.path = new LinkedList<Vertex>(u.path); // 이웃 node로 갈때, 현재 node까지의 path
-					neighbour.dest.path.add(u); // 거기다가 현재의 위치를 path에 추가 (현재 node를 거쳐가므로)
+					neighbour.dest.path = new LinkedList<Vertex>(u.path); //  path from current node to neighbour 
+					neighbour.dest.path.add(u); // add current node to path 
 
-					queue.add(neighbour.dest); // 업데이트된 이웃 node를 입력
+					queue.add(neighbour.dest); // update neighbour node
 				}
 			}
         }
 		
 		List<String> temp = new ArrayList<>();
 		
-		for(int i=0;i<vertices.get(dest).path.size(); i++) {
+		for(int i=0;i<vertices.get(dest).path.size(); i++) { // convert data type 
 			temp.add(vertices.get(dest).path.get(i).toString());
 		}
-		//List<String> temp = (LinkedList<String>)vertices.get(dest).path.clone();
-			temp.add(dest);
+			temp.add(dest); // add current node to path 
 		
 		Path path = new Path(vertices.get(dest).minDistance,temp); //new LinkedList<String>
 		
